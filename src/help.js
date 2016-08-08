d5a8716c7b48ab0175eae7ca59c3f4d7be215d8a
Jason Maurer <j@maur.co>
2016-08-08 17:30:40 -0600

    help menus

---
+++ src/help.js
@@ -0,0 +1,66 @@
+import chalk from 'chalk'
+
+export let commands = {}
+export let options = {}
+
+export let menus = {
+  _default: (data) => `
+  ${chalk.underline('Usage:')}
+
+    ${data.name} <command> [options]
+
+    ${chalk.dim('For further info about a command:')}
+    ${data.name} help <command> ${chalk.dim('or')} ${data.name} <command> --help
+
+  ${chalk.underline('Commands:')}
+
+${Object.keys(commands).map((cmd) => {
+  return '    ' + spaceOut(cmd, commands[cmd])
+}).join('\n')}
+
+  ${chalk.underline('Options:')}
+
+${Object.keys(options).map((opt) => {
+  return '    ' + spaceOut(opt, options[opt])
+}).join('\n')}
+  `
+}
+
+export function addHelpMenu (data, opts) {
+  commands[data.command] = data.description
+
+  menus[data.command] = () => {
+    const cmd = data
+
+    const description = (cmd.descriptionLong || cmd.description)
+      ? `
+  ${chalk.underline('Description:')}
+
+    ${cmd.descriptionLong || cmd.description || 'no description'}\n` : ''
+
+    const options = cmd.options
+      ? `
+  ${chalk.underline('Options:')}
+
+${Object.keys(cmd.options).map((opt) => {
+  return '    ' + spaceOut(opt, cmd.options[opt])
+}).join('\n')}\n` : ''
+
+    return `${description}
+  ${chalk.underline('Usage:')}
+
+    ${cmd.usage || `${opts.name} ${cmd.command} [options]`}
+  ${options}`
+  }
+}
+
+export function addOption (data) {
+  options[data.name] = data.description
+}
+
+export function spaceOut (name, desc) {
+  const paddingLeft = 25
+  const fill = new Array(paddingLeft - name.length - 1).join('.')
+
+  return `${name} ${chalk.dim(fill)} ${desc}`
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,49 @@
 import minimist from 'minimist'
 import chalk from 'chalk'
+import { menus, addHelpMenu, addOption } from './help'
 
 export default class Transit {
   constructor (opts) {
+    this.opts = opts
     this.cmds = {}
 
     /* parse arguments into config */
     this.argv = minimist(process.argv.slice(2), {
       boolean: ['h', 'help', 'v', 'version']
+    })
+
+    /* invalid command response */
+    const invalid = () => console.error(chalk.red(`"${this.argv._.join(' ')}" is not a valid command`))
+    this.invalidCommand = opts.invalidCommand || invalid
+  }
+
+  command (data) {
+    this.cmds[data.command] = data
+    addHelpMenu(data, this.opts)
+
+    return this
+  }
+
+  option (data) {
+    addOption(data)
+    return this
+  }
+
+  run () {
+    if (!this.cmds['help']) {
+      addHelpMenu({
+        command: 'help',
+        description: 'show help menu for a command'
+      })
+    }
+
+    addOption({
+      name: '-h, --help',
+      description: 'show help menu for a command'
+    })
+    addOption({
+      name: '-v, --version',
+      description: 'show version number'
     })
 
     /* allow help flag on any command */
@@ -17,27 +53,30 @@ export default class Transit {
 
     /* show version number */
     if (this.argv.version || this.argv.v) {
-      return console.log(`v${opts.pkg.version}`)
+      return console.log(`v${this.opts.version}` || 'no version specified')
     }
 
-    /* invalid command response */
-    const invalid = () => console.error(chalk.red(`"${this.argv._.join(' ')}" is not a valid command`))
-    this.invalidCommand = opts.invalidCommand || invalid
-  }
-
-  command (opts) {
-    this.cmds[opts.command] = opts
-  }
-
-  run () {
     const cmd = this.argv._[0]
 
     if (cmd === 'help' || cmd === undefined) {
-      return console.log('HELP')
+      return this._help(this.argv)
     } else if (this.cmds[cmd]) {
-      return this.cmds[cmd].action(this.argv)
+      return this.cmds[cmd].action({
+        raw: this.argv,
+        command: cmd
+      })
     } else {
       return Promise.reject(this.invalidCommand())
     }
+  }
+
+  _help () {
+    const cmd = this.argv._[1] || '_default'
+
+    if (this.opts.prependMenu) {
+      console.log(this.opts.prependMenu)
+    }
+
+    console.log(menus[cmd](this.opts))
   }
 }
